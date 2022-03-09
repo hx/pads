a44e0ef38ecf5a6c9961297ccea26cd03d0bebc6
Neil E. Pearson <neil.pearson@bigcommerce.com>
2022-03-09 21:26:54 +1100

    Blank gem

---
+++ lib/pads.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pads/version"
+
+module Pads
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pads_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pads do
+  it "has a version number" do
+    expect(Pads::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
